689a2e97a86198926317d80f99efc156137e0d96
Ernest DOSSA <ernestdossa.9@gmail.com>
2026-06-16 22:00:46 +0200

    feat: update config to run project with data source

--- src/main/java/com/queueflow/infrastructure/persistence/TicketJpaRepository.java
+++ src/main/java/com/queueflow/infrastructure/persistence/TicketJpaRepository.java
@@ -1,10 +1,13 @@
 package com.queueflow.infrastructure.persistence;
 
 import com.queueflow.domain.model.Ticket;
+import com.queueflow.infrastructure.persistence.entity.TicketEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Component
 public interface TicketJpaRepository
-        extends JpaRepository<Ticket, UUID> {
+        extends JpaRepository<TicketEntity, UUID> {
 }
